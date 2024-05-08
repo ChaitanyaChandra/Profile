e13a065b01ee269136d718734e00a53347eb2843
Chaitanya <56665907+ChaitanyaChandra@users.noreply.github.com>
2024-05-08 12:20:42 +0530

    Update gitp.sh

--- .customCommands/gitp.sh
+++ .customCommands/gitp.sh
@@ -2,7 +2,7 @@ FILE=~/.count
 git_push () {
       variableCount=$(cat $FILE)
       git add .
-      increment = $(expr $variableCount + 1)
+      increment=$(expr $variableCount + 1)
       echo $increment > $FILE
       git commit -m "automated commit #$variableCount"
       git push
