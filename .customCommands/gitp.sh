9e4457a8e34c1b282e3136730faf6e3ee1f7f341
Chaitanya <56665907+ChaitanyaChandra@users.noreply.github.com>
2024-05-08 11:57:06 +0530

    Update gitp.sh

--- .customCommands/gitp.sh
+++ .customCommands/gitp.sh
@@ -1,14 +1,14 @@
 FILE=~/.count
 git_push () {
-      variableCount=$(cat ~/.count)
+      variableCount=$(cat $FILE)
       git add .
-      echo $(($(cat ~/.count) + 1)) > ~/.count
+      echo $(($(cat $FILE) + 1)) > $FILE
       git commit -m "automated commit #$variableCount"
       git push
 }
 if [ -f $FILE ]; then
   git_push
 else
-  echo "0" > ~/.count
+  echo "0" > $FILE
   git_push
 fi
